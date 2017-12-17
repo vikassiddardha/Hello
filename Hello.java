a80feaa2e45416d20b2a9799e25f907f974406b1
root <root@ip-172-31-26-0.us-west-2.compute.internal>
2017-12-17 04:41:48 +0000

    change 1

--- Hello.java
+++ Hello.java
@@ -5,7 +5,7 @@ public class Hello
 		for(int i =1;i<=10;i++)
 		{
 			System.out.println("Hello World ... " +i);
-			Sysstem.out.println("hurray....! " +1);
+			Sysstem.out.println("hurray....! " +i);
 		}
 	}
 }
